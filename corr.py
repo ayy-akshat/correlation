d345c8e0d1fa86fe494e576d6c39b62d65055115
Akze <57077182+ayy-akshat@users.noreply.github.com>
2021-12-26 18:43:44 -0800

    Update corr.py

--- corr.py
+++ corr.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csv
+import pandas as pd
+import plotly.express as px
 
 with open("sample_data/coffee.csv") as file:
     data = csv.DictReader(file)
@@ -8,9 +10,13 @@ with open("sample_data/coffee.csv") as file:
     for i in data:
         x.append(float(i['Sleep']))
         y.append(float(i['Coffee']))
-    
+
     c = np.corrcoef(x, y)
     print("Correlation between sleep and coffee taken: " + str(c[0, 1]))
+
+    df = pd.read_csv("sample_data/coffee.csv")
+    d = px.scatter(df, x="Sleep", y="Coffee", title="graph", color="Week", size="Coffee")
+    d.show()
 
 with open("sample_data/marks.csv") as file:
     data = csv.DictReader(file)
@@ -19,6 +25,10 @@ with open("sample_data/marks.csv") as file:
     for i in data:
         x.append(float(i['Days Present']))
         y.append(float(i['Marks']))
-    
+
     c = np.corrcoef(x, y)
     print("Correlation between days present and marks: " + str(c[0, 1]))
+
+    df = pd.read_csv("sample_data/marks.csv")
+    d = px.scatter(df, x="Days Present", y="Marks", title="graph", color="Roll No", size="Marks")
+    d.show()
